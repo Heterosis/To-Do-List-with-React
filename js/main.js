0fe79a1f5fc2b385f612597260dd31c8ea4d9a1c
Heterosis <39578132+Heterosis@users.noreply.github.com>
2019-06-11 15:39:35 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,27 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TodoApp from './TodoApp'
 
-const todoArray = [{
-    itemName: "Buy something",
-    completed: false
-  },
-  {
-    itemName: "Do something",
-    completed: false
-  },
-  {
-    itemName: "Make something",
-    completed: false
-  },
-  {
-    itemName: "Take something",
-    completed: true
-  }
-];
-ReactDOM.render( <
-  TodoApp todos = {
-    todoArray
-  }
-  todoName = "某某人的待辦事項" / > ,
-  document.getElementById("root")
-);
+const todoArray = [
+    {
+      itemName: "Buy something",
+      completed: false
+    },
+    {
+      itemName: "Do something",
+      completed: false
+    },
+    {
+      itemName: "Make something",
+      completed: false
+    },
+    {
+      itemName: "Take something",
+      completed: true
+    }
+  ];
+  
+  ReactDOM.render(
+    <TodoApp todos={todoArray} todoName="某某人的待辦事項" />,
+    document.getElementById("root")
+  );
